9a2e5ac74b8f7da5f4ee77ca1fce56df2947e9d2
ebergel <bergel@gmail.com>
2016-08-01 07:33:39 -0300

    example

---
+++ R/example.R
@@ -0,0 +1,12 @@
+
+  
+
+rpivotNoUI::rpivotTableNoUI(   data = Titanic
+                            , rows = c("Class","Sex")
+                            , cols = c("Age","Survived" )     )
+
+rpivotTable::rpivotTable(   data = Titanic
+                           , rows = c("Class","Sex")
+                           , cols = c("Age","Survived" )      )
+
+ 
